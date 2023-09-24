0e60ba4b9c36d52a79e9d6382fed9a5678db892c
VPresich <valya.alexandrova.presich@gmail.com>
2023-09-24 19:10:34 +0200

    Рефакторинг

--- scripts/script-07-collection-products.js
+++ scripts/script-07-collection-products.js
@@ -20,7 +20,6 @@ import products from "./data/products.js";
 </article> */
 
 // Функция для создания одной карточки
-
 const makeProduct = ({name, description, price}) => {
     const productRef = document.createElement('article');
     productRef.classList.add('product');
@@ -51,30 +50,37 @@ const prodsRef = products.map(makeProduct);
 //console.log(prodsRef);
 
 // Функция для создания карточек
-const makeProductCarts = (products) => {
-    return products.map(({name, description, price}) => {
-        const productRef = document.createElement('article');
-        productRef.classList.add('product');
+// const makeProductCarts = (products) => {
+//     return products.map(({name, description, price}) => {
+//         const productRef = document.createElement('article');
+//         productRef.classList.add('product');
 
-        const titleRef = document.createElement('h2');
-        titleRef.textContent = name;
-        titleRef.classList.add('product-name');
+//         const titleRef = document.createElement('h2');
+//         titleRef.textContent = name;
+//         titleRef.classList.add('product-name');
 
-        const descriptionRef = document.createElement('p');
-        descriptionRef.textContent = description;
-        descriptionRef.classList.add('product-description');
+//         const descriptionRef = document.createElement('p');
+//         descriptionRef.textContent = description;
+//         descriptionRef.classList.add('product-description');
 
-        const priceRef = document.createElement('p');
-        priceRef.textContent = `Price: ${price} грн`;
-        priceRef.classList.add('product-price');
+//         const priceRef = document.createElement('p');
+//         priceRef.textContent = `Price: ${price} грн`;
+//         priceRef.classList.add('product-price');
 
-        productRef.append(titleRef, descriptionRef, priceRef);
+//         productRef.append(titleRef, descriptionRef, priceRef);
 
-        return productRef;
+//         return productRef;
 
-    });
-}
+//     });
+// }
 
+// Функция для создания карточек c вызовом makeProduct
+function makeProductCarts(products)
+{
+    return products.map(makeProduct);
+};
+
+    
 // Тестировагие создания массива карточек через вызов функции
 const cardsRef = makeProductCarts(products);
 //console.log(cardsRef);
